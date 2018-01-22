45e2ba175ee98ee7fdd47754ce5ce160986f0d92
Hoang Trong Tue <tueht@users.noreply.github.com>
2018-01-22 21:57:16 +0700

    Update ActionSheetPackage.java
    - fixed override compile error

--- android/src/main/java/com/slowpath/actionsheet/ActionSheetPackage.java
+++ android/src/main/java/com/slowpath/actionsheet/ActionSheetPackage.java
@@ -1,17 +1,14 @@
 package com.slowpath.actionsheet;
 
-import android.support.v4.app.FragmentActivity;
-
 import com.facebook.react.ReactPackage;
-import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
+
 
 public class ActionSheetPackage implements ReactPackage {
     @Override
@@ -19,11 +16,6 @@ public class ActionSheetPackage implements ReactPackage {
         List<NativeModule> modules = new ArrayList<>();
         modules.add(new ActionSheetModule(reactContext));
         return modules;
-    }
-
-    @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
     }
 
     @Override
